df42dae81bede84c4aa9f04007621f5bc7204d05
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-20 12:56:21 +0300

    Reactjs northwind-redux

--- SECTION6-Reactjs/northwind-redux/src/components/products/ProductList.js
+++ SECTION6-Reactjs/northwind-redux/src/components/products/ProductList.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Badge } from "reactstrap";
+import { Badge, Table } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as productActions from "../../redux/actions/productActions";
 
 class ProductList extends Component {
+  componentDidMount() {
+    this.props.actions.getProducts();
+  }
+
   render() {
     return (
       <div>
@@ -12,6 +18,26 @@ class ProductList extends Component {
             {this.props.currentCategory.categoryName}
           </Badge>
         </h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Product Name</th>
+              <th>Quantity for Per Unit</th>
+              <th>Units In Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitsInStock}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
@@ -20,7 +46,16 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    products: state.productListReducer,
   };
 }
 
-export default connect(mapStateToProps)(ProductList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
