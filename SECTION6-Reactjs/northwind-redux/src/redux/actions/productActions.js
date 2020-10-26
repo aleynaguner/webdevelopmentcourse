bdb6a0a624d0ab100d7b773d7c7dc666712b4c01
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-26 17:01:18 +0300

    React Northwind-Redux

--- SECTION6-Reactjs/northwind-redux/src/redux/actions/productActions.js
+++ SECTION6-Reactjs/northwind-redux/src/redux/actions/productActions.js
@@ -4,6 +4,52 @@ export function getProductsSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCES, payload: products };
 }
 
+export function createProductSuccess(product) {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function updateProductSuccess(product) {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" }, //not nessary for this app, because json is default content-type
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error) {
+  console.error("something went wrong ...");
+  throw error;
+}
+
 export function getProducts(categoryId) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
