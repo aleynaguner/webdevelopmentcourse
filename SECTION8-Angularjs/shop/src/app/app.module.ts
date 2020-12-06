8fa7beaaf4cc0af32abee9ff79d70efd0826e0a3
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-06 11:55:27 +0300

    Angular shop forms

--- SECTION8-Angularjs/shop/src/app/app.module.ts
+++ SECTION8-Angularjs/shop/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { AlertifyService } from './services/alertify.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
     CategoryComponent,
     ProductComponent,
     ProductFilterPipe,
+    ProductAddForms1Component,
+    ProductAddForms2Component,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [AlertifyService],
