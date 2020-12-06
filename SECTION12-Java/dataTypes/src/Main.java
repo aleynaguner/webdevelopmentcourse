1dca43416240853ebdbcedf66a27f5e9847ad582
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-06 17:19:02 +0300

    Java dataTypes

---
+++ SECTION12-Java/dataTypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n1 = 5;
+        long n2 = 1203466132;
+        char c = 'A';
+        byte b = 24;
+        boolean t = true;
+    }
+}
