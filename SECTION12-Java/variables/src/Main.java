34d94c2448ea02440b0d63414995decce2dae55b
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-06 17:07:21 +0300

    Java variables

---
+++ SECTION12-Java/variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int numberOfStudent = 15;
+        String msg = "Number of students: "
+        System.out.println(msg+numberOfStudent);
+        System.out.println(msg+numberOfStudent);
+        System.out.println(msg+numberOfStudent);
+        System.out.println(msg+numberOfStudent);
+    }
+
+}
