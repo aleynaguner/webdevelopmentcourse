697128575a7780e76c0c2c6f1f58cf7e0006b05f
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-09 23:22:38 +0300

    Java constructor

---
+++ SECTION12-Java/constructor/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Product product = new Product(5);
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
