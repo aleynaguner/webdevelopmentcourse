3feee85f2529ded8cc441506cf43b65a534fa4eb
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-10 09:55:23 +0300

    Java metodOverloading

---
+++ SECTION12-Java/methodOverloading/src/Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate {
+
+    public int Sum(int a, int b){
+        return a+b;
+    }
+    public int Sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+}
---
+++ SECTION12-Java/methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculate cl = new Calculate();
+        System.out.println(cl.Sum(5,3));
+        System.out.println(cl.Sum(5,3,5));
+    }
+}
