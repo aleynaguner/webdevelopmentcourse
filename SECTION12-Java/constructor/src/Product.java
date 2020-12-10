08ec0cff7c866b6187d4ac86aada318f496f73ea
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-10 09:54:19 +0300

    Java constructor

---
+++ SECTION12-Java/constructor/src/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+
+    //if put a parameter in constr.
+    //need to call it with that parameter
+    public Product(int id){
+        //constructor
+        //no difference with or without it
+    }
+}
