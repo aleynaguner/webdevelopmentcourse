4167bdb203e12db23a3af581413defb2663224d1
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-10 09:54:59 +0300

    Java inheritance

---
+++ SECTION12-Java/inheritance/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Customer cust = new Customer();
+        Employee emp = new Employee();
+
+        cust.age=25;
+        cust.email="adlksdashd@gmail.dom";
+
+        emp.salary=5580.45;
+    }
+}
