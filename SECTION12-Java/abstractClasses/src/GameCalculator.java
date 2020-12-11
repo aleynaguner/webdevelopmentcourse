d357aa149c4271144f9d9c7b51214c992e4948c6
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-11 23:10:12 +0300

    Java abstractClasses

---
+++ SECTION12-Java/abstractClasses/src/GameCalculator.java
@@ -0,0 +1,10 @@
+public abstract class GameCalculator {
+//    public void calculate(){
+//        System.out.println("Score is: 100");
+//    }
+    public abstract void calculate();
+
+    public final void gameOver(){
+        System.out.println("Game is over");
+    }
+}
