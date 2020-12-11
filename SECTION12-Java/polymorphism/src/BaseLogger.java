28dcbf7f67f705c7c8a030c0586d41a70a7829b8
Aleyna Güner <aleynaguner@hotmail.com>
2020-12-11 22:53:04 +0300

    Java override (final keyword)

--- SECTION12-Java/polymorphism/src/BaseLogger.java
+++ SECTION12-Java/polymorphism/src/BaseLogger.java
@@ -2,4 +2,8 @@ public class BaseLogger {
     public void log(String message){
         System.out.println(message);
     }
+    //with final keyword you can not override this method other classes
+    public final void write(String message){
+        System.out.println(message);
+    }
 }
--- SECTION12-Java/polymorphism/src/DatabaseLogger.java
+++ SECTION12-Java/polymorphism/src/DatabaseLogger.java
@@ -2,4 +2,9 @@ public class DatabaseLogger extends BaseLogger{
     public void log(String message){
         System.out.println("Log to database: " + message);
     }
+
+//    public void write(String message) {
+//        System.out.println(message);
+//    }
+
 }
